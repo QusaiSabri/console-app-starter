ecb77e9520e71bd8d96dc63d178386d2a94f2dab
Qusai Sabri <qusai.sabri19@gmail.com>
2024-05-30 18:11:31 -0500

    Add using IHost , following Microsoft docs recommendation

--- src/ConsoleAppStarter/Program.cs
+++ src/ConsoleAppStarter/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
-var hostBuilder = CreateHostBuilder(args).Build();
+using IHost hostBuilder = CreateHostBuilder(args).Build();
 
 using var scope = hostBuilder.Services.CreateScope();
 
